6be7fcf82fc84657b8ea1b9b714c106c710015fe
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-03-27 23:27:51 -0300

    Updates

---
+++ precache-manifest.9b07f6956ff295464edbd1264edf508a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d7712e32e0c927d3a939353bc4b46d",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "fe24ba158b329189f9bd",
+    "url": "/portofolio/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "20cb958c19aa8839c888",
+    "url": "/portofolio/static/css/main.57659aca.chunk.css"
+  },
+  {
+    "revision": "fe24ba158b329189f9bd",
+    "url": "/portofolio/static/js/2.2c7c426c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portofolio/static/js/2.2c7c426c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20cb958c19aa8839c888",
+    "url": "/portofolio/static/js/main.08651546.chunk.js"
+  },
+  {
+    "revision": "f686ac747f44e078fda9",
+    "url": "/portofolio/static/js/runtime-main.4103e0cc.js"
+  },
+  {
+    "revision": "af76f70accce5dadaf14b2fd2bdd09ff",
+    "url": "/portofolio/static/media/imagem1.af76f70a.png"
+  },
+  {
+    "revision": "38a677b5f718ca7db961ed7f0e6c3ea8",
+    "url": "/portofolio/static/media/primary_foto.38a677b5.png"
+  }
+]);
